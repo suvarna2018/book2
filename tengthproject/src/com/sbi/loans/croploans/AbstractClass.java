f7a5bfa41739484280e4b885e74be276c69705be
suvarna2018 <suvarnashankarp@gmail.com>
2018-05-28 10:51:22 +0530

    added a new project
    addea a new project

---
+++ tengthproject/src/com/sbi/loans/croploans/AbstractClass.java
@@ -0,0 +1,9 @@
+package com.sbi.loans.croploans;
+
+public abstract class AbstractClass {
+	int a;
+	public void m1() {
+		System.out.println("m1 in abstractclass");
+	}
+	public abstract void m2();
+}
---
+++ tengthproject/src/com/sbi/loans/croploans/ChidClass.java
@@ -0,0 +1,18 @@
+package com.sbi.loans.croploans;
+
+public class ChidClass extends AbstractClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ChidClass obj=new ChidClass();
+		obj.m1();
+		obj.m2();
+	}
+
+	@Override
+	public void m2() {
+		// TODO Auto-generated method stub
+		System.out.println("m2 override in abstractclass");
+	}
+
+}
